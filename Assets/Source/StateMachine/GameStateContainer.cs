f887faab644b2543503c8c17c7649ab4fd80fa36
Miaan <nghiabn2005@gmail.com>
2024-01-27 07:40:13 +0700

    Message Display

--- Assets/Source/StateMachine/GameStateContainer.cs
+++ Assets/Source/StateMachine/GameStateContainer.cs
@@ -1,6 +1,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(
+    fileName = "StateContainer", menuName = "Scriptable Objects/StateContainer", order = 1
+)]
 public class GameStateContainer : ScriptableObject
 {
     public List<TextAsset> Data;
--- Assets/Source/StateMachine/GameStateController.cs
+++ Assets/Source/StateMachine/GameStateController.cs
@@ -1,6 +1,40 @@
 using UnityEngine;
+using Ink.Runtime;
 
 public class GameStateController : MonoBehaviour
 {
+    [SerializeField]
+    private GameStateContainer _stateContainer;
 
+    private Story _currentStory;
+
+    public bool ChangeState(string stateName)
+    {
+        string data = "";
+        if (GetState(stateName, out data))
+        {
+            Debug.Log($"State data not found");
+            return false;
+        }
+
+        _currentStory = new Story(data);
+        Debug.Log(data);
+
+        return true;
+    }
+
+    private bool GetState(string stateName, out string data)
+    {
+        foreach (TextAsset stateData in _stateContainer.Data)
+        {
+            if (stateData.name == stateName)
+            {
+                data = stateData.text;
+                return true;
+            }
+        }
+
+        data = "";
+        return false;
+    }
 }
